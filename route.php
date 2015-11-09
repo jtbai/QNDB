c9ad82885315ddda98bb8cb50399b940434f26eb
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2015-11-08 22:32:09 -0500

    Ajout de la base du MVC

--- route.php
+++ route.php
@@ -1,62 +1,64 @@
 <?php
 
-if(isset($_POST['Controller'])){
-$Controller = $_POST['Controller'];
-    if(isset($_POST['Action']))
+if (isset($_POST['Controller'])) {
+    $Controller = $_POST['Controller'];
+    if (isset($_POST['Action']))
         $Action = $_POST['Action'];
     else
         $Action = "PUT";
-}elseif(isset($_GET['Controller'])){
+} elseif (isset($_GET['Controller'])) {
     $Controller = $_GET['Controller'];
-    if(isset($_GET['Action']))
+    if (isset($_GET['Action']))
         $Action = $_GET['Action'];
     else
         $Action = "GET";
-}else{
+} else {
     $Controller = "Default";
     $Action = "GET";
 }
 
-if(isset($_GET['ID'])){
+if (isset($_GET['ID'])) {
     $ID = $_GET['ID'];
-}else{
+} else {
     $ID = NULL;
 }
 
 
-switch($Controller){
+switch ($Controller) {
 
-    CASE "family":{
+    CASE "family": {
         $ControllerInstance = new family_controller();
         BREAK;
     }
 
-    CASE "household":{
+    CASE "household": {
         $ControllerInstance = new household_controller();
         BREAK;
     }
 
-    CASE "Default":{
+    CASE "Default": {
         $ControllerInstance = NULL;
         BREAK;
     }
 
 }
 
-switch($Action){
+if (!is_null($ControllerInstance)) {
+    switch ($Action) {
 
-    CASE "GET":{
-        $ControllerInstance->GET($ID);
-        BREAK;
-    }
+        CASE "GET": {
+            $ControllerInstance->GET($ID);
+            BREAK;
+        }
 
-    CASE "POST":{
-        $ControllerInstance->POST(HTML::PreparePostData($_POST,'FORM'));
-        BREAK;
-    }
+        CASE "POST": {
+            $ControllerInstance->POST(HTML::PreparePostData($_POST, 'FORM'));
+            BREAK;
+        }
 
-    CASE "PUT":{
-        $ControllerInstance->PUT(HTML::PreparePostData($_POST,'FORM'));
-        BREAK;
+        CASE "PUT": {
+            $ControllerInstance->PUT(HTML::PreparePostData($_POST, 'FORM'));
+            BREAK;
+        }
     }
 }
