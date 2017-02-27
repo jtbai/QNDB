7c6d1900d10f3e317a943fbd22e701937ad5ebcf
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2017-02-27 18:24:23 -0500

    Correction pour la deplacements des periodes. on doit prendre la session en cours (pas tous les shifts au monde)

--- action_case_modifperiode.php
+++ action_case_modifperiode.php
@@ -6,12 +6,13 @@ foreach($_POST['FORMIDPiscine'] as $k=>$v){
 	$Req = "SELECT DISTINCT IDPeriode, Jour, Start FROM periode WHERE IDPiscine=$k and Semaine+Jour*(60*60*24)+Start >".$SD." AND Semaine+Jour*(60*60*24)+Start <".$ED." GROUP BY Semaine, Jour, Start ORDER BY Semaine ASC, Jour ASC, Start ASC";
 	$SQL->SELECT($Req);
 	while($Rep = $SQL->FetchArray()){
-		$Req2 = "SELECT max(Semaine) FROM periode WHERE Jour=".$Rep['Jour']." AND Start=".$Rep['Start']." AND IDPiscine = ".$k;
+		$Req2 = "SELECT max(Semaine) FROM periode WHERE Jour=".$Rep['Jour']." AND Start=".$Rep['Start']." AND IDPiscine = ".$k." AND IDSession=".$_ACTIVE['Session'] ;
 		$SQL2->SELECT($Req2);
 		$Rep2 = $SQL2->FetchArray();
 		$ToWeek =$Rep2[0]+60*60*24*7;
 		$Req2 = "UPDATE periode SET Semaine=".$ToWeek." WHERE IDPeriode=".$Rep['IDPeriode'];
 		$SQL2->UPDATE($Req2);
+		print($Req2);
 	}
 }
 
