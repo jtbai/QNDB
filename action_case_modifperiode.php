2b0e171e8001790415d349a475ba2c75285c693c
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-02-11 12:27:10 -0500

    On remet le meme comportement que SDS pour la gestion des dates de début et fin (donc jusqua insclusif)

--- action_case_modifperiode.php
+++ action_case_modifperiode.php
@@ -3,7 +3,7 @@ $SQL2 = new sqlclass;
 $SD = mktime(0,0,0,$_POST['MultiVar_START4'],$_POST['MultiVar_START5'],$_POST['MultiVar_START3']);
 $ED = mktime(0,0,0,$_POST['MultiVar_END4'],$_POST['MultiVar_END5'],$_POST['MultiVar_END3']);
 foreach($_POST['FORMIDPiscine'] as $k=>$v){
-	$Req = "SELECT DISTINCT IDPeriode, Jour, Start FROM periode WHERE IDPiscine=$k and Semaine+Jour*(60*60*24)+Start >".$SD." AND Semaine+Jour*(60*60*24)+Start <".$ED." GROUP BY Semaine, Jour, Start ORDER BY Semaine ASC, Jour ASC, Start ASC";
+	$Req = "SELECT DISTINCT IDPeriode, Jour, Start FROM periode WHERE IDPiscine=$k and Semaine+Jour*(60*60*24)+Start >".$SD." AND Semaine+Jour*(60*60*24)+Start <=".$ED." GROUP BY Semaine, Jour, Start ORDER BY Semaine ASC, Jour ASC, Start ASC";
 	$SQL->SELECT($Req);
 	while($Rep = $SQL->FetchArray()){
 		$Req2 = "SELECT max(Semaine) FROM periode WHERE Jour=".$Rep['Jour']." AND Start=".$Rep['Start']." AND IDPiscine = ".$k;
